5717e96f33d01cb57101c95358720bcf35fbef2f
Patryk Peszko <comes.de@gmail.com>
2011-07-24 22:30:25 +0200

    handling multiple directories

--- rubynator.rb
+++ rubynator.rb
@@ -1,11 +1,15 @@
-dir = ARGV[0]
+dirs = ARGV
+
+dirs_with_code_lines = ARGV.map do |arg|
+  `grep "def " -r #{arg}`
+end
 
 methods = []
-lines_with_methods = `grep "def " -r #{dir}`
-
-lines_with_methods.split("\n").map do |method_name|
-  if method_name =~ /.*def (self\.)?(\w*)(\(.*\))?/
-    methods << $2
+dirs_with_code_lines.each do |dir_with_lines|
+  dir_with_lines.split("\n").map do |method_name|
+    if method_name =~ /.*def (self\.)?(\w*)(\(.*\))?/
+      methods << $2
+    end
   end
 end
 
@@ -15,7 +19,7 @@ p methods
 
 puts "potentialy not used methods:"
 methods.each do |method_name|
-  results = `egrep --color "[^_]#{method_name}[^_]?" -r #{dir} | egrep --color -v "def "`
+  results = `egrep --color "[^_]#{method_name}[^_]?" -r #{dirs.join(' ')} | egrep --color -v "def "`
   if results == ''
     p method_name
   end
